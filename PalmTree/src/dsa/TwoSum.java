736c14e0ec62ea4eea7d94123ac92edd37317f09
AnkitDedha <ankitdedha211995@gmail.com>
2023-02-07 00:56:24 +0530

    6feb23

---
+++ PalmTree/src/dsa/TwoSum.java
@@ -0,0 +1,24 @@
+package dsa;
+
+import java.util.ArrayList;
+
+
+public class TwoSum {
+
+	// Problem: find a number pair in an array whose sum is k
+	// input is array of integers = {2,1,4,3,5}
+	// output is , if k = 4, {1,3}
+	public Integer[] twoSumSoln(int[] arr , int k) {
+		
+		int[] ar = {1,3,2,4,6,5};
+		arr = ar;
+		
+		// we iterate through and save element if k - element is not in hashmap
+		
+		
+		
+		return null;
+		
+	}
+	
+}
